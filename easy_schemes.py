6ce90c24f10443c8c9151466afe200b89e9c3904
mcscope <mcscope@gmail.com>
2017-06-06 00:24:17 -0700

    Add fullscan scheme

--- easy_schemes.py
+++ easy_schemes.py
@@ -1,12 +1,37 @@
 from scheme import Scheme
 
 
-class MaxWhite(Scheme):
+class Solid(Scheme):
+    # abstract base
+    def setall(self, color):
+        for led in range(self.strip.num_leds):
+            self.strip.smart_set_pixel(*color)
+
+
+class MaxWhite(Solid):
     pause_between_updates = 60
 
     def init():
-        for led in range(self.strip.num_leds):
-            self.strip.set_pixel_rgb(0xFFFFF, 100)
+        self.setall(0xFFF, 0xFFF, 0xFFF)
 
     def paint():
         return False
+
+
+class FullScan(Solid):
+    pause_between_updates = 0.001
+    color = [0, 0, 0]
+    color_step = [1, 0, 0]
+
+    def init():
+        self.setall(self.color)
+
+    def paint():
+        color = [val + step for val, step in zip(self.color, self.color_step)]
+
+        if max(self.color) > 0xFFF:
+            self.color = [0, 0, 0]
+            self.color_step.append(self.color_step.pop(0))
+
+        self.setall(color)
+        return True
