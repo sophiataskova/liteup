835a40788871a86f4abca2b3d61429341d98c062
mcscope <mcscope@gmail.com>
2017-06-08 10:48:07 -0700

    refactor linear interpration

--- base_schemes.py
+++ base_schemes.py
@@ -21,8 +21,13 @@ class InterpolateScheme(Scheme):
     def paint_lin_interp(self, led_num, start_color, target_color, steps=10):
         for cur_step in range(steps):
             stepcolor = [
-                int(start_val + ((target_Val - start_val) * (cur_step / steps)))
-                for start_val, target_Val in zip(start_color, target_color)
+                lin_interp(step, steps, start_val, target_val)
+                for start_val, target_val in zip(start_color, target_color)
             ]
-            self.strip.set_pixel(led_num, stepcolor)
+            self.strip.set_pixel(led_num, *stepcolor)
             yield True
+
+    @staticmethod
+    def lin_interp(step, total_steps, start_val, target_val):
+
+        return int(start_val + ((target_val - start_val) * (cur_step / steps)))
