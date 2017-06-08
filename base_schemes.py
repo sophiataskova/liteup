ed0b837fc0b21caecfba8bf198daa561008bc288
mcscope <mcscope@gmail.com>
2017-06-08 10:50:38 -0700

    add tests

--- base_schemes.py
+++ base_schemes.py
@@ -21,13 +21,14 @@ class InterpolateScheme(Scheme):
     def paint_lin_interp(self, led_num, start_color, target_color, steps=10):
         for cur_step in range(steps):
             stepcolor = [
-                lin_interp(step, steps, start_val, target_val)
+                lin_interp(cur_step, steps, start_val, target_val)
                 for start_val, target_val in zip(start_color, target_color)
             ]
+            print(stepcolor)
             self.strip.set_pixel(led_num, *stepcolor)
             yield True
 
     @staticmethod
-    def lin_interp(step, total_steps, start_val, target_val):
+    def lin_interp(cur_step, num_steps, start_val, target_val):
 
-        return int(start_val + ((target_val - start_val) * (cur_step / steps)))
+        return int(start_val + ((target_val - start_val) * (cur_step / num_steps)))
--- scheme_tests.py
+++ scheme_tests.py
@@ -8,7 +8,10 @@ from base_schemes import InterpolateScheme
 class InterpolateSchemeTester(unittest.TestCase):
 
     def test_lin_interp(self):
-        self.assertEqual(lin_interp(6, 10, 0, 10), 6)
+        self.assertEqual(InterpolateScheme.lin_interp(6, 10, 0, 10), 6)
+        self.assertEqual(InterpolateScheme.lin_interp(7, 10, 0, 10), 7)
+        # hit rounding
+        self.assertEqual(InterpolateScheme.lin_interp(6, 10, 0, 12), 7)
 
 if __name__ == '__main__':
     unittest.main()
