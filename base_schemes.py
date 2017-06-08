202617a6f199f8bc0ec8abb3c046f30fea619a6b
mcscope <mcscope@gmail.com>
2017-06-08 17:26:39 +0000

    adding base and twinkle schemes

---
+++ base_schemes.py
@@ -0,0 +1,28 @@
+from scheme import Scheme
+
+
+class Solid(Scheme):
+    # abstract base
+    def setall(self, color):
+        for led in range(self.strip.num_led):
+            self.strip.set_pixel(led, *color)
+
+
+class GeneratorScheme(Scheme):
+    def init(self):
+        self.gen = self.generator()
+
+    def paint(self):
+        return next(self.gen)
+
+
+class InterpolateScheme(Scheme):
+
+    def paint_lin_interp(self, led_num, start_color, target_color, steps=10):
+        for cur_step in range(steps):
+            stepcolor = [
+                int(start_val + ((target_Val - start_val) * (cur_step / steps)))
+                for start_val, target_Val in zip(start_color, target_color)
+            ]
+            self.strip.set_pixel(led_num, stepcolor)
+            yield True
---
+++ twinkle_scheme.py
@@ -0,0 +1,8 @@
+from base_schemes import GeneratorScheme
+
+
+class TwinkleScheme(GeneratorScheme):
+    base_color = [1, 1, 1, 1]
+
+    def generator(self):
+        pass
